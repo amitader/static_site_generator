ed0a6c2927d56a9377205f8da1f5ad0aa94c7047
amitader <amit.adler12@gmail.com>
2025-05-01 15:18:34 +0300

    creating site

--- src/main.py
+++ src/main.py
@@ -1,23 +1,29 @@
 import os
 import shutil
+import sys
 from copystatic import transfer_dir
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_docs ="./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
+    try:
+        basepath=sys.argv[1]
+    except:
+        basepath="/"
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    transfer_dir(dir_path_static,dir_path_public)
+    transfer_dir(dir_path_static,dir_path_docs)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
 
 
     
